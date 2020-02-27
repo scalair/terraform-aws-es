817300018d01277ae26d11a3d169333c12485269
Adel Abdelhak <6298183+blakelead@users.noreply.github.com>
2020-02-27 22:06:11 +0100

    Add VPC only access

--- main.tf
+++ main.tf
@@ -6,14 +6,7 @@ resource "aws_security_group" "es" {
     from_port   = 0
     to_port     = 65535
     protocol    = "TCP"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = data.terraform_remote_state.vpc.outputs.private_subnets_cidr_blocks
   }
 
   tags = var.aws_security_group_es_tags
